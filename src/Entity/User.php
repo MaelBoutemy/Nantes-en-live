36a741434a19426844928b04afd66647785f7cb9
Thibault <thibault.leminoux@gmail.com>
2019-02-19 12:48:52 +0100

    Password Form
    suécurisation password + flavicon header

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
@@ -33,11 +34,13 @@ class User
 
     /**
      * @ORM\Column(type="date")
+     *
      */
     private $birthdate;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
      */
     private $password;
 
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,11 +6,18 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Length;
 
 class UserType extends AbstractType
 {
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -20,9 +27,20 @@ class UserType extends AbstractType
             ->add('birthdate', DateType::class, [
                 'widget' => 'single_text',
             ])
-            ->add('password')
-        ;
+            ->add('password', PasswordType::class, [
+                'constraints' => [
+        new NotBlank([
+            'message' => 'Please enter a password',
+        ]),
+        new Length([
+            'min' => 6,
+            'minMessage' => 'Your password should be at least {{ limit }} characters',
+            'max' => 4096
+        ]),
+    ],
+            ]);
     }
+
 
     public function configureOptions(OptionsResolver $resolver)
     {
