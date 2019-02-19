a72319b363a6bfeb60f656dd20e71001d0a79912
Thibault <thibault.leminoux@gmail.com>
2019-02-19 16:08:27 +0100

    CRUD Article

---
+++ src/Form/ArticleType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('created_At')
+            ->add('updated_At')
+            ->add('content')
+            ->add('state')
+            ->add('author')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+        ]);
+    }
+}
