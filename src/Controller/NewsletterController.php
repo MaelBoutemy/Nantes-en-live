6e3b8f1d00adee61a47c085177e1d46f4f6208f5
mael <maelboutemy@hotmail.fr>
2019-02-19 16:03:59 +0100

    04-newsletter-footer

--- src/Controller/NewsletterController.php
+++ src/Controller/NewsletterController.php
@@ -4,29 +4,93 @@ namespace App\Controller;
 
 use App\Entity\Newsletter;
 use App\Form\NewsletterType;
+use App\Repository\NewsletterRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/newsletter")
+ */
 class NewsletterController extends AbstractController
 {
     /**
-     * @Route("/newsletter", name="newsletter_new", methods={"GET","POST"})
+     * @Route("/", name="newsletter_index", methods={"GET"})
+     */
+    public function index(NewsletterRepository $newsletterRepository): Response
+    {
+        return $this->render('newsletter/index.html.twig', [
+            'newsletters' => $newsletterRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="newsletter_new", methods={"GET","POST"})
      */
     public function new(Request $request): Response
     {
-        $user = new Newsletter();
-        $form = $this->form->create(NewsletterType::class, $user);
+        $newsletter = new Newsletter();
+        $form = $this->createForm(NewsletterType::class, $newsletter);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $this->Doctrine->persist($user);
-            $this->Doctrine->flush();
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($newsletter);
+            $entityManager->flush();
 
-            return $this->redirectToRoute('user_index');
+            return $this->redirectToRoute('newsletter_index');
         }
-        return $form;
 
+        return $this->render('newsletter/new.html.twig', [
+            'newsletter' => $newsletter,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="newsletter_show", methods={"GET"})
+     */
+    public function show(Newsletter $newsletter): Response
+    {
+        return $this->render('newsletter/show.html.twig', [
+            'newsletter' => $newsletter,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="newsletter_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Newsletter $newsletter): Response
+    {
+        $form = $this->createForm(NewsletterType::class, $newsletter);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('newsletter_index', [
+                'id' => $newsletter->getId(),
+            ]);
+        }
+
+        return $this->render('newsletter/edit.html.twig', [
+            'newsletter' => $newsletter,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="newsletter_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Newsletter $newsletter): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$newsletter->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($newsletter);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('newsletter_index');
     }
 }
---
+++ src/Form/AdminType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Admin;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AdminType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName')
+            ->add('lastName')
+            ->add('password')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Admin::class,
+        ]);
+    }
+}
