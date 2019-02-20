f7f45716a55618880f4568f6b528e7395e00adbd
mael <maelboutemy@hotmail.fr>
2019-02-20 10:22:03 +0100

    Created search bar

---
+++ src/Controller/SearchController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SearchController extends AbstractController
+{
+    /**
+     * @Route("/search", name="search")
+     */
+    public function index()
+    {
+        return $this->render('search/index.html.twig', [
+            'controller_name' => 'SearchController',
+        ]);
+    }
+
+    public function searchBar()
+    {
+        $form =  $this->createFormBuilder(NULL)
+            ->add('query', TextType::class)
+            ->add('search', SubmitType::class, [
+                'attr' => [
+                    'class' => 'btn btn-primary'
+                ]
+            ])
+            ->getForm();
+
+        return $this->render('search/search.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
